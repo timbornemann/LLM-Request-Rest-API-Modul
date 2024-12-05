ded38dae2adca7e6b2b26dcaa0fe108f0c7d6598
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-12-05 12:54:54 +0100

    Füge Testfälle ein

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,42 @@ public class Main {
 
         LanguageModelClient client = new LanguageModelClient();
 
+/*
+        //list models
         try {
-            client.generateResponse("Warum ist der himmel blau?", false);
+         for(String e : client.listModels()){
+             System.out.println(e);
+         }
         } catch (IOException e) {
            e.printStackTrace();
         }
-
-
+*/
+/*
+        //list running models
+        try {
+            for(LanguageModelClient.Model m : client.listRunningModels()){
+                System.out.println(m.getModelName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+*/
+        /*
+        //load model
+        try {
+           client.loadModel(LanguageModelClient.Model.LLAMA3_2_3B);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+*/
+/*
+        //chat
+        try {
+         String s =   client.generateResponse("warum ist der himmerl blau", false);
+            System.out.println(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+*/
     }
 }
